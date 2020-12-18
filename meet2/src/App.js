0ed6b3d1aaac0f4b6beb8ac9bdbd60154faf6d40
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-18 14:31:00 +0700

    up

--- meet2/src/App.js
+++ meet2/src/App.js
@@ -1,40 +1,35 @@
-import './App.css';
-import React from 'react';
-import PropTypes from 'prop-types';
-
-class App extends React.Component {
-    constructor(props) {
-       super(props);
-       this.state = {
-          header: "Header from props...",
-          content: "Content from props..."
-       }
-    }
-    render() {
-       return (
-          <div>
-             <Header headerProp = {this.state.header}/>
-             <Content contentProp = {this.state.content}/>
-          </div>
-       );
-    }
- }
- class Header extends React.Component {
-    render() {
-       return (
-          <div>
-             <h1>{this.props.headerProp}</h1>
-          </div>
-       );
-    }
- }
- class Content extends React.Component {
-    render() {
-       return (
-          <div>
-             <h2>{this.props.contentProp}</h2>
-          </div>
-       );
-    }
- }
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'; 
+import Home from './component/home.js'; 
+import About from './component/about.js'; 
+import Contact from './component/contact.js'; 
+import './App.css'; 
+  
+class App extends Component { 
+  render() { 
+    return ( 
+       <Router> 
+           <div className="App"> 
+            <ul className="App-header"> 
+              <li> 
+                <Link to="/">Home</Link> 
+              </li> 
+              <li> 
+                <Link to="/about">About Us</Link> 
+              </li> 
+              <li> 
+                <Link to="/contact">Contact Us</Link> 
+              </li> 
+            </ul> 
+            <Switch> 
+              <Route exact path='/' component={Home}></Route> 
+              <Route exact path='/about' component={About}></Route> 
+              <Route exact path='/contact' component={Contact}></Route> 
+            </Switch> 
+          </div> 
+       </Router> 
+   ); 
+  } 
+} 
+  
 export default App;
