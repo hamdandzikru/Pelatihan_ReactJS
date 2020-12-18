644b6c9b77d7e0a07b496de7351dfa60a3c19607
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-18 14:47:35 +0700

    up

--- meet2/src/component/contact.js
+++ meet2/src/component/contact.js
@@ -1,12 +1,12 @@
 import React from 'react'; 
   
 function Contact (){ 
- return <address> 
-            You can find us here:<br /> 
-            GeeksforGeeks<br /> 
-            5th & 6th Floor, Royal Kapsons, A- 118, <br /> 
-            Sector- 136, Noida, Uttar Pradesh (201305) 
-        </address> 
+    return <address> 
+    Alamat:<br /> 
+    Gedung C<br /> 
+    Fakultas Rekayasa Industri <br /> 
+    Bandung (pinggiran) 
+</address> 
 } 
   
 export default Contact; 
